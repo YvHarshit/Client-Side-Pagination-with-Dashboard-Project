9d46f389c1adde3d00e2a2d2a3e9287fcdf6f488
YvHarshit <yvharshit@gmail.com>
2026-07-27 15:37:35 +0530

    Add in main.tsx => axios.defaults.baseURL

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import 'react-toastify/dist/ReactToastify.css'
+import axios from "axios";
+
+axios.defaults.baseURL =
+import.meta.env.VITE_BACKEND_URL;
+
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   
