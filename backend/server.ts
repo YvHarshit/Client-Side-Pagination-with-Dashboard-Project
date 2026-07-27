dd873a72d5745beb7404df09064e3f75d4773a89
YvHarshit <yvharshit@gmail.com>
2026-07-27 12:18:13 +0530

    Fully checked

--- backend/server.ts
+++ backend/server.ts
@@ -1,61 +1,61 @@
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import express, { type Application } from "express";
-// import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { type Application } from "express";
+import mongoose from "mongoose";
 import userRoutes from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
-// import cookieParser from "cookie-parser";
-// import "./cron/lateMarkCron.js"
-// import "./cron/autoCheckoutCron.js"
-
-// dotenv.config();
-// const app: Application = express();
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin:"http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-
-// app.use("/api", userRoutes);
-// app.use("/api/auth", authRouter);
-
-// mongoose.connect(process.env.MONGO_URL as string)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch((err: Error) => console.error(err));
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });;
-
-
-import express from "express";
-import cors from "cors";
 import cookieParser from "cookie-parser";
+import "./cron/lateMarkCron.js"
+import "./cron/autoCheckoutCron.js"
 
-const app = express();
+dotenv.config();
+const app: Application = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://your-frontend-domain.vercel.app",
-  ],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin:"http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api", userRoutes);
 app.use("/api/auth", authRouter);
 
+mongoose.connect(process.env.MONGO_URL as string)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err: Error) => console.error(err));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`);
+});;
+
+
+// import express from "express";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://your-frontend-domain.vercel.app",
+//   ],
+//   credentials: true,
+// }));
+
+// app.use("/api", userRoutes);
+// app.use("/api/auth", authRouter);
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Listening on ${PORT}`);
+// });
