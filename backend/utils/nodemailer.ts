616a47f33bd49012b8a718bf0163dc03a5a6d0d9
YvHarshit <yvharshit@gmail.com>
2026-07-27 13:24:45 +0530

    Nodemailer Config : port changes to : 465

--- backend/utils/nodemailer.ts
+++ backend/utils/nodemailer.ts
@@ -5,8 +5,8 @@ dotenv.config() ;
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
-    // port: 587,
-    // secure: false, // true only for port 465
+   port: 465,
+   secure: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD
