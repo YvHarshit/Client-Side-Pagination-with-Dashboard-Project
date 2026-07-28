d23618ec54a8d33855a26a8d2ced0289704d5eff
YvHarshit <yvharshit@gmail.com>
2026-07-28 17:21:25 +0530

    vite.config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-      target: 'http://localhost:3000',
+      target: "https://client-side-pagination-with-dashboard.onrender.com",
       changeOrigin: true,
       },
     },
